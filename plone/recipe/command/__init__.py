7ccc1b901b17e93371f3185bd52a6849a3325cbb
wichert <wichert@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2007-10-02 12:44:18 +0000

    Add some parameter validation and a proper error message

--- plone/recipe/command/__init__.py
+++ plone/recipe/command/__init__.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import zc.buildout
 
 class Recipe:
     def __init__(self, buildout, name, options):
@@ -7,8 +8,13 @@ class Recipe:
         self.logger = logging.getLogger(name)
 
     def install(self):
-        command = self.options['command']
-        self.logger.info("Running %s" % command)
+        command = self.options.get('command', None)
+        if not command:
+            msg="No command specified"
+            self.logger.error(msg)
+            raise zc.buildout.UserError(msg)
+
+        self.logger.info("Running '%s'" % command)
         os.system(command)
         location = self.options.get('location')
         if location is not None:
